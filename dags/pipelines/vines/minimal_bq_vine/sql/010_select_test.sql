e67c8d9f78c4b4ae949bc644cf91a6858780c74d
Matthew L. <ujlim.korea@gmail.com>
2026-08-20 14:48:54 +0900

    Fix sample SQL business date parameter

--- dags/pipelines/vines/minimal_bq_vine/sql/010_select_test.sql
+++ dags/pipelines/vines/minimal_bq_vine/sql/010_select_test.sql
@@ -1,6 +1,6 @@
 -- A harmless query used to verify SQL execution and named parameters.
 SELECT
-  @batch_date AS batch_date,
+  @business_date AS business_date,
   CURRENT_TIMESTAMP() AS executed_at,
   'minimal_bq_vine' AS vine_id,
   'run_select_sql' AS grape_id;
